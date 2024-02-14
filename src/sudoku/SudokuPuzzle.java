cab558473a46f783ce510cc745b3ee916ce75bcf
zmletters <zemingletters@gmail.com>
2024-02-14 15:46:32 +0800

    added comments

--- src/sudoku/SudokuPuzzle.java
+++ src/sudoku/SudokuPuzzle.java
@@ -23,6 +23,7 @@ public class SudokuPuzzle {
 	}
 	
 	public void displayGrid() {
+		// Displaying the grid in console
 		for (int i=0;i<9;i++) {
 			for (int j=0;j<9;j++) {
 				System.out.print(grid[i][j]);
@@ -33,5 +34,12 @@ public class SudokuPuzzle {
 	
 	public void generatePuzzle() {
 		int n1 = random.nextInt(9) + 1;
+	}
+	
+	public boolean checkRepeat(int i, int j) {
+		// Takes in two constraints i = row position, j column position
+		// This function will check if there is repeating number in the same row, column and 3x3 box
+		
+		return true;
 	}
 }
